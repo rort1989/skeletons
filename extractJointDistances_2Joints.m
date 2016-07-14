d519dd0588f25fe6573ede183d58ecd3e04abe1c
rort1989 <zhaorort@gmail.com>
2016-07-14 00:10:09 -0400

    change the JointOrientations for kinect v2 joint indices

--- extractJointDistances_2Joints.m
+++ extractJointDistances_2Joints.m
@@ -2,13 +2,13 @@
 %FILE:      extractJointDistances_2Joints.m
 %AUTHOR:    Kosta Andoni (kosta.andoni@gmail.com)
 %DATE:      4.12.2016
+%REVISION: Rui Zhao
+%REVISION DATE: 7.13.2016
 %PURPOSE:   Calculate the joint distance vector between two joints in their
 %           respective frames
 %************************************************************************
 %INPUTS:    jt_locs - cell array containing the joint locations
 %           sequence - sequence of interest
-%           frame_A - frame of joint_A
-%           frame_B - frame of joint_B
 %           joint_A,joint_B - joints of interest
 %
 %OUTPUTS:   jt_dist - vector magnitude of the distance between the
@@ -19,19 +19,12 @@
 %************************************************************************
 
 function [jt_dist,jt_dist_vec]...
-    = extractJointDistances_2Joints(jt_locs,sequence,frame_A,frame_B,joint_A,joint_B)
+    = extractJointDistances_2Joints(jt_locs,sequence,joint_A,joint_B)
 
 %Calculate the distance in the X,Y,Z axes
-jt_dist_x = jt_locs{sequence,1}{1,frame_B}(joint_B,1)-jt_locs{sequence,1}{1,frame_A}(joint_A,1);
-jt_dist_y = jt_locs{sequence,1}{1,frame_B}(joint_B,2)-jt_locs{sequence,1}{1,frame_A}(joint_A,2);
-jt_dist_z = jt_locs{sequence,1}{1,frame_B}(joint_B,3)-jt_locs{sequence,1}{1,frame_A}(joint_A,3);
+jt_dist_vec = jt_locs{sequence,1}(3*joint_B-2:3*joint_B,:)-jt_locs{sequence,1}(3*joint_A-2:3*joint_A,:); % 3*T matrix
 
 %Apply distance formula
-jt_dist = sqrt((jt_dist_x.^2)+(jt_dist_y.^2)+(jt_dist_z.^2));
-
-%Vector form of distance
-jt_dist_vec(1,1) = jt_dist_x;
-jt_dist_vec(1,2) = jt_dist_y;
-jt_dist_vec(1,3) = jt_dist_z;
+jt_dist = sqrt(sum(jt_dist_vec.^2)); % 1*T vector
 
 end
--- loaddata.m
+++ loaddata.m
@@ -1,46 +1,59 @@
-function [data, id_sub, labels] = loaddata(dir, subject, gesture, instance, subsample)
-% Load MSR_Action3D_Skeleton data belong to the same action
-% load data and store them in a f*m*n matrix where n is the number of 
-% instances, m is the dimension of data unit, f is the number of frames
+function [data, labels] = loaddata(dir, gesture, subject, instance, pose, source, subsample)
+% Load selected color/depth/skeleton data
+% load data and store them in an N*1 array of cells where N is the number of 
+% instances
 % Input: 
-%       subject: a vector of indices of subject who perform the gesture
+%       dir: indicate the folder name where the data is stored
 %       gesture: a vector of indices gesture type 
+%       subject: a vector of indices of subject who perform the gesture       
 %       instance: a vector of indices for instances of each gesture
+%       pose: a vector of indices for pose of each gesture
+%       source: 1: color; 2: depth; 3: skeleton
+%       subsample: downsampling factor number of each sequence (default: 1)
 % Output: 
-%       data: an array of cells, each has D*T matrix, where D=joints dimension, T=# frames
-%       minframe: f value
-%       id_sub: an array of length be total number of instances indicate
-%               subject index of each instance
+%       data: an array of N*1 cells, each has D*T matrix, where D=joints dimension, T=# frames
+%       labels: a N*4 matrix, first column is index of gesture type, second
+%               column is index of subject; third column is index of
+%               instance; fourth column is index of pose
 
-if nargin < 5
+if nargin < 7
     subsample = 1;
 end
-data = cell(1000,1);
-id_sub = zeros(1000,1);
-labels = zeros(1000,1);
+N = length(gesture)*length(subject)*length(instance)*length(pose);
+data = cell(N,1);
+labels = zeros(N,4);
 count = 0;
 for a = gesture
     for s = subject
         for e = instance
-            filename = strcat(dir,sprintf('a%02i_s%02i_e%02i_skeleton.txt',a,s,e));
-            if exist(filename,'file') % only load existed files
-                I = load(filename,'ascii');
-                nframe = size(I,1)/20;
-                I = I(:,1:3);
-                I = reshape(I',3*20,nframe); % I is 60*nframe matrix, each column is the one feature vector at one time frame
-                % feature change along axis first then joints
-                I_clip = I(:,1:subsample:end); % subsample every instance to minframe*60
-                % the whole matrix is just one instance of the gesture
-                count = count + 1;
-                data{count} = I_clip; 
-                id_sub(count) = s;
-                labels(count) = a;
+            for p = pose
+                switch source
+                    case 1 % not processed yet
+                        filename = strcat(dir,'/',sprintf('a%02i_s%02i_e%02i_p%02i_color.mat',a,s,e,p));
+                    case 2 % not processed yet
+                        filename = strcat(dir,'/',sprintf('a%02i_s%02i_e%02i_p%02i_depth.mat',a,s,e,p));
+                    otherwise
+                        filename = strcat(dir,'/',sprintf('a%02i_s%02i_e%02i_p%02i_skeleton.mat',a,s,e,p));
+                        if exist(filename,'file') % only load existed files
+                            var = 'skeleton';
+                            I = load(filename,var);
+                            I = I.(var);
+                            nframe = size(I,1)/25; % number of joints = 25 for kinect v2
+                            I = I(:,1:3);
+                            I = reshape(I',3*25,nframe); % I is 60*nframe matrix, each column is the one feature vector at one time frame
+                            % feature change along axis first then joints
+                            I_clip = I(:,1:subsample:end); % subsample every instance to minframe*60
+                            % the whole matrix is just one instance of the gesture
+                            count = count + 1;
+                            data{count} = I_clip;
+                            labels(count,:) = [a s e p];
+                        end
+                end
             end
         end
     end
 end
 data = data(1:count);
-id_sub = id_sub(1:count);
-labels = labels(1:count);
+labels = labels(1:count,:);
 
 end
